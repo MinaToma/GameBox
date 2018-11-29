485a4ccf99c1fb519b91ab55be44e456f1c31372
MinaToma <39103197+MinaToma@users.noreply.github.com>
2018-11-29 14:16:17 +0200

    Hello World Commit

---
+++ app/src/main/java/com/example/mina/gamebox/SolitairGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.mina.gamebox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SolitairGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solitair_game);
+    }
+}
